bd793e4b0ea5599d61df62e441c5cd2065106e51
Sean <mail@sgaffney.me>
2026-07-12 09:10:07 -0400

    Removed elevation from database

---
+++ RunScope.Core/Migrations/20260711131459_RemoveElevation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunScope.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveElevation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ElevationGainMeters",
+                table: "Routes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "ElevationGainMeters",
+                table: "Routes",
+                type: "float",
+                nullable: true);
+        }
+    }
+}
